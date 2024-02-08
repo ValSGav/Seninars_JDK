33d6f7e40ec36432ce59c505b22dead5a77e5ee4
ValSGav <valentin-s-gavrilov@yandex.ru>
2024-02-08 10:37:56 +0300

    Initial commit

---
+++ src/main/java/seninar1_HomeWork/Main.java
@@ -0,0 +1,10 @@
+package seninar1_HomeWork;
+
+import seninar1_HomeWork.client.ClientWindow;
+
+public class Main {
+    public static void main(String[] args) {
+        ClientWindow wnd = new ClientWindow("OKOSHKO");
+        wnd.setVisible(true);
+    }
+}
---
+++ src/main/java/seninar1_HomeWork/client/ClientWindow.java
@@ -0,0 +1,33 @@
+package seninar1_HomeWork.client;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class ClientWindow extends JFrame {
+
+    private boolean isConnectedToServer;
+    private ArrayList<String> messages;
+
+    private boolean sendMessage(String text){
+        if (isConnectedToServer){
+
+        };
+        return false;
+    };
+
+    public boolean acceptMessage(){
+       return false;
+    };
+
+    private boolean connectToServer(){
+        //загрузить историю сообщений
+        //установить признак соединения
+        return false;
+    };
+
+    public ClientWindow(String title) throws HeadlessException {
+        super(title);
+    }
+
+}
