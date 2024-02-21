ea8c199e19954434b82584545d9e4641c5636a97
Val Gavrilov <valentin-s-gavriliv@yandex.ru>
2024-02-21 23:06:06 +0300

    seminar3_homeWork_3

--- src/main/java/seminar3/task1/homeWork/ArraysComparator.java
+++ src/main/java/seminar3/task1/homeWork/ArraysComparator.java
@@ -2,15 +2,14 @@ package seminar3.task1.homeWork;
 
 public class ArraysComparator {
     public <O, P> boolean compareArrays(O[] array1, P[] array2) {
-        if (array1.getClass().getName().equals(array2.getClass().getName())
-                && array1.length == array2.length) {
+        if (array1.length == array2.length) {
 
             // я случайно подсмотрел начало следующего семинара
             // тоже не понял, что достаточно проверить на совпадение типов
-            /*for (int i = 0; i < array1.length; i++) {
-                if (!array1[i].equals(array2[i]))
+            for (int i = 0; i < array1.length; i++) {
+                if (!array1[i].getClass().getName().equals(array2[i].getClass().getName()))
                     return false;
-            }*/
+            }
             return true;
         } else {
             return false;
